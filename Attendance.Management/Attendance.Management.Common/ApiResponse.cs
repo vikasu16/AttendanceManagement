4b168f02fb1a86149085eff80a43e51cc918c5e8
vikasu16 <vikas84uu@gmail.com>
2022-11-13 21:46:52 +0530

    Added new common project for fetching Common Response Data in all Layers of Project

---
+++ Attendance.Management/Attendance.Management.Common/ApiResponse.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attendance.Management.Common
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; }
+        public object Response { get;  }
+
+        public ApiResponse(int statusCode, string message = null, object response = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+            Response = response;
+        }
+
+        private static string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 200:
+                    return "Succuess in fetching data";
+                case 404:
+                    return "Resource not found";
+                case 500:
+                    return "An unhandled error occurred";
+                case 401:
+                    return "You are not authorized for this request";
+                default:
+                    return null;
+            }
+        }
+    }
+}
